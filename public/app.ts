785d72543de015734ef7e039f2d5d931203eda5f
Zlvsky <zlvsky@icloud.com>
2022-07-30 18:16:38 +0200

    Changed app.js to app.ts, fixed some logic

--- public/app.ts
+++ public/app.ts
@@ -1,43 +1,34 @@
-const input = document.getElementById('link'),
-      submit = document.getElementById('Convert'),
-      add = document.getElementById('add'),
-      count = document.getElementById('counts');
+const input = document.getElementById('link') as HTMLInputElement,
+      submit = document.getElementById('Convert') as HTMLButtonElement,
+      addButton = document.getElementById('add') as HTMLButtonElement,
+      count = document.getElementById('counts') as HTMLSpanElement;
 
-var multipleVideos = [];
+var multipleVideos: Array<string> = [];
 var counting = 0;
 var firstReg = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/gi;
 var secondReg = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
 
-add.onclick = () => {
-  let inputval = input.value;
-  let preID = inputval.match(firstReg).toString();
-  let finalID = preID.match(secondReg)[7];
-  multipleVideos.push(finalID);
-  input.value = ''
-  counting++;
-  count.innerHTML = counting;
+if (addButton != null) {
+  addButton.onclick = () => {
+    if (input != null) {
+      const inputValue = input.value;
+      const preID = inputValue.match(firstReg).toString();
+      const finalID = preID.match(secondReg)[7];
+      multipleVideos.push(finalID);
+      input.value = ''
+      counting++;
+      count.innerHTML = `${counting}`;
+    }
+  }
 }
 submit.onclick = () => {
   counting = 0;
-  count.innerHTML = counting;
+  count.innerHTML = `${counting}`;
 
   const xhr = new XMLHttpRequest();
   xhr.open("POST" ,"convert-yt-mp3", true);
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  xhr.send(multipleVideos);
+  xhr.send(JSON.stringify(multipleVideos));
 
-  // fetch("/convert-yt-mp3", {
-  //   method: "POST",
-  //   headers: { "Content-Type": "application/json" },
-  //   body: multipleVideos,
-  // }).then((res) => {
-  //   console.log("Request complete! response:", res);
-  // });
-
-  // (async () => {
-  //     const linki = `links/${tablica}`;
-  //     const response = await fetch(linki);
-  //     // console.log(json);
-  // })();
   multipleVideos = [];
 }
